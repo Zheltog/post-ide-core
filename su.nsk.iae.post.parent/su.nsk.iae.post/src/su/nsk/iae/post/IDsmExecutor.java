e13e6a4297cd8932d2ca74397ba0a2783a13dec0
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-04-18 16:13:30 +0700

    Доработки по IDsmExecutor

--- su.nsk.iae.post.parent/su.nsk.iae.post/src/su/nsk/iae/post/IDsmExecutor.java
+++ su.nsk.iae.post.parent/su.nsk.iae.post/src/su/nsk/iae/post/IDsmExecutor.java
@@ -1,13 +1,15 @@
 package su.nsk.iae.post;
 
+import java.util.LinkedHashMap;
+
 /**
 * Interface to be implemented in any DSM-core for DSM-wrapper could invoke execution of DSM-core logic
 */
 public interface IDsmExecutor {
 	/**
 	* Starts DSM-core logic execution
-	* @param request represents request for DSM to work with
+	* @param request parameters map for DSM to work with
 	* @return result message
 	*/
-	String execute(Object request);
+	String execute(LinkedHashMap<String, Object> request);
 }
